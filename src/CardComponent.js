798cf0e8151d5b60f6c4424e24e27cc0e4437ccf
lmpko <lmpkoo@gmail.com>
2023-05-17 17:19:10 +0200

    css for card added

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -3,24 +3,27 @@ import logo from "/images/logo.svg"
 const CardComponent = () => {
   return (
     <section>
-        <div>
+        <div className='balance-preview'>
             <div>
                 <span>My balance</span>
-                <p>$921.48</p>
+                <span>$921.48</span>
             </div>
             <img src={logo} alt="logo" />
         </div>
-        <div>
+        <div className='chart-preview'>
             <h1>Spending - Last 7 days</h1>
             <hr/>
-            <div>
-                <span> Total this month</span>
-                <p>$478.33</p>
+            <div className='chart-summary'>
+                <div className='this-month'>
+                    <span> Total this month</span>
+                    <span>$478.33</span>
+                </div>
+                <div className='last-month'>
+                    <span> +2.4%</span>
+                    <span>from last month</span>
+                </div>
             </div>
-            <div>
-                <span> +2.4%</span>
-                <p>from last month</p>
-            </div>
+            
         </div>
     </section>
   )
